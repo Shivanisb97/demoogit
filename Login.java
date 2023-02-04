da3a4e500549fc091c001b23e68a83b485e34823
Your Name <you@example.com>
2023-02-04 10:59:54 +0530

    new file

--- Login.java
+++ Login.java
@@ -3,8 +3,9 @@ package project;
 public class Login {
 
 	public static void main(String[] args) {
-		
-
+		System.out.println("*************************");
+		System.out.println("this is new project");
+		System.out.println("*************************");
 	}
 
 }
